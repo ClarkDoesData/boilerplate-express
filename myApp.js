65b8101e665aa1cd8066fa706060c0c23db6e820
Clark Montano <clrk.montano@gmail.com>
2024-10-09 16:04:19 +0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,7 +3,15 @@ let app = express();
 
 console.log("Hello World")
 
+/*
+app.get('/', (request, response) => {
+    response.send('Hello Express');
+});
+*/
 
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + "/views/index.html");
+});
 
 
 
